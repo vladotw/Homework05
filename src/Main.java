9063ee5a8461951a346b9f377d76bfa501ef29e4
Vlad <vladot@ya.ru>
2022-12-06 16:41:20 +0600

    Task1, Task2, Task3, Task4, Task5 (Corrected)

--- src/Main.java
+++ src/Main.java
@@ -97,42 +97,27 @@ public class Main {
         int monthNumber = 12;
 
             switch (monthNumber) {
+                case 12:
                 case 1:
-                    System.out.println("Зима");
-                    break;
                 case 2:
                     System.out.println("Зима");
                     break;
                 case 3:
-                    System.out.println("Весна");
-                    break;
                 case 4:
-                    System.out.println("Весна");
-                    break;
                 case 5:
                     System.out.println("Весна");
                     break;
                 case 6:
-                    System.out.println("Лето");
-                    break;
                 case 7:
-                    System.out.println("Лето");
-                    break;
                 case 8:
                     System.out.println("Лето");
                     break;
                 case 9:
-                    System.out.println("Осень");
-                    break;
                 case 10:
-                    System.out.println("Осень");
-                    break;
                 case 11:
                     System.out.println("Осень");
                     break;
-                case 12:
-                    System.out.println("Зима");
-                    break;
+
                 default: System.out.println("Ошибка, такого месяца не существует");
             }
     }
