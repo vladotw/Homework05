6080efead055ab8bfda754c0cf2bc9df848fd931
Vlad <vladot@ya.ru>
2022-12-06 15:26:35 +0600

    Task1, Task2, Task3

--- src/Main.java
+++ src/Main.java
@@ -30,6 +30,20 @@ public class Main {
         System.out.println("Задача_2");
         System.out.println();
 
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if (clientOS == 1) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS ==1) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
         System.out.println();
         System.out.println();
@@ -39,6 +53,15 @@ public class Main {
 
         System.out.println("Задача_3");
         System.out.println();
+        int year = 2021;
+
+        if (year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else if ((year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
 
 
         System.out.println();
