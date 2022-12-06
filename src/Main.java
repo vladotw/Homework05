17629447ab039f1dffd6668f377c3290005dc028
Vlad <vladot@ya.ru>
2022-12-06 13:01:44 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
 
+    }
+
+    public static void task1() {
+
+        System.out.println("Задача_1");
+        System.out.println();
+
+        char clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task2() {
+
+        System.out.println("Задача_2");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task3() {
+
+        System.out.println("Задача_3");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task4() {
+
+        System.out.println("Задача_4");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task5() {
+
+        System.out.println("Задача_5");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
     }
 }
